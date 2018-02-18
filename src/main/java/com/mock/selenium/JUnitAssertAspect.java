78319e2aeb2a37ef8745a8644e8bd8bca089a6bd
Yuvaraj <yuvaraj.gunisetti@ba.com>
2018-02-18 00:59:33 +0000

    updated the junit assert statements

--- src/main/java/com/mock/selenium/JUnitAssertAspect.java
+++ src/main/java/com/mock/selenium/JUnitAssertAspect.java
@@ -1,9 +1,8 @@
 package com.mock.selenium;
 
-import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Pointcut;
 
 /**
  * Created by Yuvaraj on 16/02/2018.
@@ -11,13 +10,8 @@ import org.aspectj.lang.annotation.Pointcut;
 @Aspect
 public class JUnitAssertAspect {
 
-//    @Pointcut("call(* org.junit.Assert.assert*(..))")
-//    public void assertMethods(ProceedingJoinPoint joinPoint) {
-//        return;
-//    }
-
-//    @Pointcut("call(* org.junit.Assert.fail(..))")
-//    public void fail(ProceedingJoinPoint joinPoint) throws Throwable {
-//        joinPoint.proceed();
-//    }
+    @Around("call(* org.junit.Assert.assertEquals(..))")
+    public void assertMethods(ProceedingJoinPoint jp) {
+        System.out.println("calling assert equals");
+    }
 }
